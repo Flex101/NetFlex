5664102f47e2078380829d0fe36192d2df1a41b3
Flex101 <msgoodliffe@gmail.com>
2016-12-27 21:36:07 +0000

    fixup! CSS FIXES

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 clear
-echo Building...
+echo Building CSS...
+sass -C grails-app/assets/stylesheets/style.scss:grails-app/assets/stylesheets/style.css
+echo DONE!
+echo Building Code...
 echo 
 ./grailsw war ROOT.war -Dgrails.env=production
 
