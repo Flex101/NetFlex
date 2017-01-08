992c35ae584d91211c2b3e68c1408d58ea3449a5
Flex101 <msgoodliffe@gmail.com>
2017-01-08 21:24:34 +0000

    BUG FIXES

--- grails-app/controllers/streama/MyListController.groovy
+++ grails-app/controllers/streama/MyListController.groovy
@@ -2,7 +2,9 @@ package streama
 
 import grails.converters.JSON
 import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
 
+@Transactional(readOnly = true)
 class MyListController {
 
 	static responseFormats = ['json', 'xml']
